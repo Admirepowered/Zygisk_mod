dde426d062ebbfae51d8adc4afb966f07df72942
Admire <admire1971@outlook.com>
2024-04-10 23:16:28 +0800

    umount update

--- loader/src/injector/unmount.cpp
+++ loader/src/injector/unmount.cpp
@@ -11,6 +11,7 @@ using namespace std::string_view_literals;
 namespace {
     constexpr auto MODULE_DIR = "/data/adb/modules";
     constexpr auto KSU_OVERLAY_SOURCE = "KSU";
+    constexpr auto AP_OVERLAY_SOURCE = "APatch";
     const std::vector<std::string> KSU_PARTITIONS{"/system", "/vendor", "/product", "/system_ext", "/odm", "/oem"};
 
     void lazy_unmount(const char* mountpoint) {
@@ -85,17 +86,40 @@ void revert_unmount_magisk() {
     }
 }
 void revert_unmount_apatch() {
+    std::string ksu_loop;
     std::vector<std::string> targets;
 
-    // Unmount dummy skeletons and MAGISKTMP
-    // since mirror nodes are always mounted under skeleton, we don't have to specifically unmount
+    // Unmount ksu module dir last
+    targets.emplace_back(MODULE_DIR);
+
     for (auto& info: parse_mount_info("self")) {
+        if (info.target == MODULE_DIR) {
+            ksu_loop = info.source;
+            continue;
+        }
         // Unmount everything mounted to /data/adb
         if (info.target.starts_with("/data/adb")) {
             targets.emplace_back(info.target);
         }
+        // Unmount ksu overlays
+        if (info.type == "overlay"
+            && info.source == AP_OVERLAY_SOURCE
+            && std::find(KSU_PARTITIONS.begin(), KSU_PARTITIONS.end(), info.target) != KSU_PARTITIONS.end()) {
+            targets.emplace_back(info.target);
+        }
+        // Unmount temp dir
+        if (info.type == "tmpfs" && info.source == AP_OVERLAY_SOURCE) {
+            targets.emplace_back(info.target);
+        }
+    }
+    for (auto& info: parse_mount_info("self")) {
+        // Unmount everything from ksu loop except ksu module dir
+        if (info.source == ksu_loop && info.target != MODULE_DIR) {
+            targets.emplace_back(info.target);
+        }
     }
 
+    // Do unmount
     for (auto& s: reversed(targets)) {
         lazy_unmount(s.data());
     }
