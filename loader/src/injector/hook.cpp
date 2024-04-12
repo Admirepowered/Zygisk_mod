c35d7d19a3834bfab6575dfc7d5b6ebeb541c2d9
Admire <admire1971@outlook.com>
2024-04-12 22:22:59 +0800

    add auto unmount

--- loader/src/injector/hook.cpp
+++ loader/src/injector/hook.cpp
@@ -584,6 +584,9 @@ void ZygiskContext::run_modules_post() {
 void ZygiskContext::app_specialize_pre() {
     flags[APP_SPECIALIZE] = true;
     info_flags = zygiskd::GetProcessFlags(g_ctx->args.app->uid);
+    if ((info_flags & PROCESS_ON_DENYLIST)==PROCESS_ON_DENYLIST){
+        flag[DO_REVERT_UNMOUNT] = true;
+    }
     if ((info_flags & (PROCESS_IS_MANAGER | PROCESS_ROOT_IS_MAGISK)) == (PROCESS_IS_MANAGER | PROCESS_ROOT_IS_MAGISK)) {
         LOGI("current uid %d is manager!", g_ctx->args.app->uid);
         setenv("ZYGISK_ENABLED", "1", 1);
