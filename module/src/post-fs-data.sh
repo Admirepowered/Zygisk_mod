8a255c400b3ed47f16731c151ab4a9f305969389
Admire <admire1971@outlook.com>
2024-05-20 23:53:33 +0800

    p_net0

--- module/src/post-fs-data.sh
+++ module/src/post-fs-data.sh
@@ -28,7 +28,7 @@ create_sys_perm() {
 
 export TMP_PATH=/sbin
 [ -d /sbin ] || export TMP_PATH=/debug_ramdisk
-
+[ ! -e /data/adb/litemode ] || export TMP_PATH=/dev/net0
 create_sys_perm $TMP_PATH
 
 if [ -f $MODDIR/lib64/libzygisk.so ];then
